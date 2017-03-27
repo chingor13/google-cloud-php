69aad2ae71ed10a2d18c9f064a48338f98695d95
Jeff Ching <chingor@google.com>
2017-03-27 09:20:06 -0700

    Fix GAE env variable parsing

--- src/Trace/RequestTracer.php
+++ src/Trace/RequestTracer.php
@@ -236,8 +236,8 @@ class RequestTracer
             self::HTTP_CLIENT_PROTOCOL => ['SERVER_PROTOCOL'],
             self::HTTP_USER_AGENT => ['HTTP_USER_AGENT'],
             self::HTTP_HOST => ['HTTP_HOST', 'SERVER_NAME'],
-            self::GAE_APP_MODULE => 'GAE_SERVICE',
-            self::GAE_APP_VERSION => 'GAE_VERSION'
+            self::GAE_APP_MODULE => ['GAE_SERVICE'],
+            self::GAE_APP_VERSION => ['GAE_VERSION']
         ];
         foreach ($labelMap as $labelKey => $headerKeys) {
             $val = array_reduce($headerKeys, function ($carry, $headerKey) use ($headers) {
